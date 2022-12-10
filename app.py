df7d57aa9837ca99da6f9b35e7e76e4e940b8f80
unknown <jonathannebu@gmail.com>
2022-12-10 13:11:26 +0530

    upload image option in posture detection

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template,url_for,Response,request
+from flask import Flask, render_template,url_for,Response,request, flash, redirect
 import os
 import cv2
 import datetime,time
 import numpy as np
-from threading import Thread
+from werkzeug.utils import secure_filename
+
+allowed_formats = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploadedimages')
 
 global capture, switch, out 
 capture=0
 switch=1
 camera = cv2.VideoCapture(0)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_formats
 
 def gen_frames():  # generate frame by frame from camera
     global out, capture
@@ -21,7 +30,7 @@ def gen_frames():  # generate frame by frame from camera
                 capture=0
                 now = datetime.datetime.now()
                 #to save the image in the pc
-                p = os.path.sep.join(['capture', "capture_{}.png".format(str(now).replace(":",''))])
+                p = os.path.sep.join(['static/capture', "capture_{}.png".format(str(now).replace(":",''))])
                 cv2.imwrite(p, frame)
             
          
@@ -61,10 +70,6 @@ def capture_pose():
 
 
 
-@app.route('/detection')
-def detection():
-    pass
-
 @app.route('/chronic')
 def chronic():
     return render_template('Mainpages/chronic.html')
@@ -76,6 +81,26 @@ def benefits():
 @app.route('/preventionchronic')
 def preventionchronic():
     pass
+
+@app.route('/detection', methods = ['POST','GET'])
+def detection():
+    full_filename = ""
+    if request.method == 'POST':
+        for file in os.listdir('static/uploadedimages'):
+          os.remove('static/uploadedimages/' + file)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename) 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('Mainpages/detection.html', uploaded_image = full_filename)
 
 
 @app.route('/requests',methods=['POST','GET'])
